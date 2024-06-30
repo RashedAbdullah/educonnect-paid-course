d9a6f2bce2dfe1ef24961f5fe9dc4d4cb9dc3287
RashedAbdullah <maarashed40@gmail.com>
2024-06-30 15:52:06 +0600

    Some files

--- app/login/page.js
+++ app/login/page.js
@@ -1,5 +1,18 @@
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div>
+      <iframe
+        src="https://embedstreams.me/t20/t20-world-cup-final-south-africa-vs-india-stream-4"
+        width="100%"
+        height="100%"
+        scrolling="no"
+        frameborder="0"
+        allowfullscreen
+        allowtransparency
+        referrerpolicy="unsafe-url"
+      ></iframe>
+    </div>
+  );
 };
 
 export default LoginPage;
